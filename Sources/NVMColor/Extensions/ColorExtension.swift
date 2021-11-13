0ee37c45793e7a25a813617292a36f905c175ba3
Damian Van de Kauter <vdkdamian@gmail.com>
2021-11-13 10:42:10 +0100

    Init hex

--- Sources/NVMColor/Extensions/ColorExtension.swift
+++ Sources/NVMColor/Extensions/ColorExtension.swift
@@ -8,6 +8,28 @@
 import SwiftUI
 
 public extension Color {
+    
+    init(_ hex: String) {
+        let hex = hex.cleanedHex
+        var int: UInt64 = 0
+        
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (1, 1, 1, 0)
+        }
+        
+        self.init(.sRGB, red: Double(r) / 255, green: Double(g) / 255, blue:  Double(b) / 255, opacity: Double(a) / 255)
+    }
+    
     var hex: String? {
         let colorString = "\(self)"
         if let colorHex = colorString.isHex() {
@@ -36,7 +58,7 @@ public extension Color {
                 if (g > 1.0) {g = 1.0}
                 if (b > 1.0) {b = 1.0}
                 
-                let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+                let rgb: Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
                 return String(format: "#%06X", rgb).cleanedHex
             } else {
                 return nil
