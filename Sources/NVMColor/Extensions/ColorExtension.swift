4c4974718da850089d9837c6ce1f8924717a4a6e
Damian Van de Kauter <vdkdamian@gmail.com>
2022-04-19 21:42:55 +0200

    Print

--- Sources/NVMColor/Extensions/ColorExtension.swift
+++ Sources/NVMColor/Extensions/ColorExtension.swift
@@ -16,6 +16,7 @@ extension Color {
      */
     public init?(hex: String?) {
         guard let hexString = hex else { return nil }
+        print("hex: \(hex)")
         
         let r, g, b, a: CGFloat
         let cleanedHex = hexString.cleanedHex
@@ -23,8 +24,9 @@ extension Color {
         if cleanedHex.count == 8 {
             let scanner = Scanner(string: cleanedHex)
             var hexNumber: UInt64 = 0
-
+            
             if scanner.scanHexInt64(&hexNumber) {
+                print("hexNumber: \(hexNumber)")
                 r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
                 g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
                 b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
