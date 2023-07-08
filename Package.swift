80cf6140a29a88197f9bee51918843d706dc6034
Damian Van de Kauter <vdkdamian@gmail.com>
2023-07-08 20:55:59 +0200

    feat: watchOS

--- Package.swift
+++ Package.swift
@@ -6,7 +6,10 @@ import PackageDescription
 let package = Package(
     name: "NVMColor",
     platforms: [
-        .macOS(.v11), .iOS(.v13), .tvOS(.v14)
+        .macOS(.v11),
+        .iOS(.v13),
+        .tvOS(.v14),
+        .watchOS(.v7)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
